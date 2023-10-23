3700475154a3873657eb029634d1b846c7cb0b1a
Jay Mandane <jvergel.mandane@gmail.com>
2023-10-23 15:17:20 +0800

    Wall Drawing #11

---
+++ wd011.js
@@ -0,0 +1,69 @@
+/*
+ * Wall Drawing #11 (1969)
+ * 
+ * A wall divided horizontally and vertically into four equal parts. Within
+ * each part, three of the four kinds of lines are superimposed.
+ */
+
+const CANVAS_SIZE = 600
+const INC_SIZE = 10
+// Should be a factor of CANVAS_SIZE (and CANVAS_SIZE / 2) for best results
+
+function setup()
+{
+    createCanvas(CANVAS_SIZE, CANVAS_SIZE)
+}
+
+function draw()
+{
+    stroke(204, 204, 204)
+
+    for (var v = 0; v <= CANVAS_SIZE; v += INC_SIZE)
+    {
+        line(v, 0, v, CANVAS_SIZE)
+    }
+    for (var h = 0; h <= CANVAS_SIZE; h += INC_SIZE)
+    {
+        line(0, h, CANVAS_SIZE, h)
+    }
+
+    stroke('red')
+    for (var q1 = 0; q1 <= CANVAS_SIZE / 2; q1 += INC_SIZE)
+    {
+        line(0, q1, q1, 0)
+    }
+    for (var q1 = 0; q1 <= CANVAS_SIZE / 2; q1 += INC_SIZE)
+    {
+        line(CANVAS_SIZE / 2, q1, q1, CANVAS_SIZE / 2)
+    }
+
+    stroke('blue')
+    for (var q2 = CANVAS_SIZE / 2; q2 <= CANVAS_SIZE; q2 += INC_SIZE)
+    {
+        line(q2, 0, CANVAS_SIZE, CANVAS_SIZE - q2)
+    }
+    for (var q2 = 0; q2 <= CANVAS_SIZE / 2; q2 += INC_SIZE)
+    {
+        line(CANVAS_SIZE / 2, q2, CANVAS_SIZE - q2, CANVAS_SIZE / 2)
+    }
+
+    stroke('darkgoldenrod')
+    for (var q3 = 0; q3 <= CANVAS_SIZE / 2; q3 += INC_SIZE)
+    {
+        line(q3, CANVAS_SIZE / 2, CANVAS_SIZE / 2, CANVAS_SIZE - q3)
+    }
+    for (var q3 = CANVAS_SIZE / 2; q3 <= CANVAS_SIZE; q3 += INC_SIZE)
+    {
+        line(0, q3, CANVAS_SIZE - q3, CANVAS_SIZE)
+    }
+
+    stroke('green')
+    for (var q4 = CANVAS_SIZE / 2; q4 <= CANVAS_SIZE; q4 += INC_SIZE)
+    {
+        line(CANVAS_SIZE / 2, q4, q4, CANVAS_SIZE / 2)
+    }
+    for (var q4 = CANVAS_SIZE / 2; q4 <= CANVAS_SIZE; q4 += INC_SIZE)
+    {
+        line(q4, CANVAS_SIZE, CANVAS_SIZE, q4)
+    }
+}
